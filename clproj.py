09275c93d158bfc6616cf0f74bd727b9fbcd5707
Clare Lavin <clarelavinie@yahoo.ie>
2021-08-21 11:52:57 +0100

    added explanatory lines

--- clproj.py
+++ clproj.py
@@ -13,5 +13,9 @@ import matplotlib.pyplot as plt
 #import val_data.csv, ignoring the first 24 rows
 val_data = pd.read_csv(r"C:\Users\clare\OneDrive\Documents\Clare\Cert in Introductory Data analytics\Weather data\Val_data.csv", skiprows=24)
 print(val_data.head())
+
+#View column names and data types of each
 print(val_data.info())
+
+#checking the number of rows and colums
 print(val_data.shape)
