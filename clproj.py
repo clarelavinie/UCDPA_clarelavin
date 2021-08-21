0c83dc701e4072e09595b571f1023d0168f6502f
Clare Lavin <clarelavinie@yahoo.ie>
2021-08-21 11:47:17 +0100

    started file

---
+++ clproj.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 11:00:42 2021
+
+@author: clare
+"""
+
+# import pandas, numpy and matplotlib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#import val_data.csv, ignoring the first 24 rows
+val_data = pd.read_csv(r"C:\Users\clare\OneDrive\Documents\Clare\Cert in Introductory Data analytics\Weather data\Val_data.csv", skiprows=24)
+print(val_data.head())
+print(val_data.info())
+print(val_data.shape)
