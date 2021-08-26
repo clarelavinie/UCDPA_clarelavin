31b407ca447654d3404adc88f5d52cc5e677d704
Clare Lavin <clarelavinie@yahoo.ie>
2021-08-26 12:44:47 +0100

    plot for rain days and season column added

--- codetrys.py
+++ codetrys.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Import data for Valencia
+#Import data for Valentia
 val_data_temp = pd.read_csv(r"C:\Users\clare\OneDrive\Documents\Clare\Cert in Introductory Data analytics\Weather data\Dub_airport_weather_data_July.csv", skiprows=25)
 print(val_data_temp.head())
 
@@ -23,7 +23,7 @@ val_data_temp["date"] - pd.offsets.DateOffset(years=100)
 val_data_temp["date"] -= pd.offsets.DateOffset(years=100)
 print(val_data_temp.head())
 
-#Import weather data for Valencia
+#Import weather data for Valentia
 val_data = pd.read_csv(r"C:\Users\clare\OneDrive\Documents\Clare\Cert in Introductory Data analytics\Weather data\Val_data.csv", skiprows=24)
 print(val_data.info())
 
@@ -70,7 +70,7 @@ print(rain_data.describe())
 #Correct years from 1942 to 1970
 #removed code as doesn't work
 
-#create cumulative rainfall for Valencia and Dublin Airport
+#create cumulative rainfall for Valentia and Dublin Airport
 #correct date formate attempt 2
 dates = pd.to_datetime(rain_data.date, infer_datetime_format=True)
 print(dates)
@@ -92,7 +92,7 @@ rain_data["raindays_dub"] = result
 print(rain_data)
 
 
-#add a column to indicate if rain day in Valencia
+#add a column to indicate if rain day in Valentia
 result = []
 for value in rain_data["rain_val"]:
     if value > 0.2:
@@ -105,7 +105,7 @@ print(rain_data)
 
 rain_data.sum(axis=0)
 
-#add a column to indicate if heavy rain day in Valencia
+#add a column to indicate if heavy rain day in Valentia
 result = []
 for value in rain_data["rain_val"]:
     if value > 10.0:
@@ -140,51 +140,49 @@ annual_rain_data = annual_rain_data.iloc[:-1,:]
 print(annual_rain_data.head())
 print(annual_rain_data.info())
 
-#Add a 20 year moving average field for Valencia and Dublin
+#Add a 20 year moving average field for Valentia and Dublin
 annual_rain_data['MA_20_Dub'] = annual_rain_data["rain_dub"].expanding(min_periods=20).mean()
 annual_rain_data['MA_20_Val'] = annual_rain_data["rain_val"].expanding(min_periods=20).mean()
 print(annual_rain_data.head(21))
 
 
 #plot the yearly rainfall amounts
-timeline = annual_rain_data()
 fig, ax = plt.subplots()
-ax.plot(timeline.index, timeline["rain_dub"], label='Dub')
-ax.plot(timeline.index, timeline["rain_val"], label='Val')
-ax.plot(timeline.index, timeline["MA_20_Val"], label='20yr MA Val', linestyle='dashed')
-ax.plot(timeline.index, timeline["MA_20_Dub"], label='20yr MA Dub', linestyle='dashed')
-ax.set_xlabel('Year')
-ax.set_ylabel('Annual Rainfall (mm)')
-ax.set_title("Annual Rainfall, 1942 to 2020: Valencia and Dublin stations")
-plt.legend()
-plt.show()
-
-#Group rainfall days by year
-fig, ax = plt.subplots()
-ax.plot(annual_rain_data.index, annual_rain_data["rain_dub"], label='Dub')
-ax.plot(annual_rain_data.index, annual_rain_data["rain_val"], label='Val')
+ax.plot(annual_rain_data.index, annual_rain_data["rain_dub"], marker="o", label='Dub')
+ax.plot(annual_rain_data.index, annual_rain_data["rain_val"], marker="s", label='Val')
 ax.plot(annual_rain_data.index, annual_rain_data["MA_20_Val"], label='20yr MA Val', linestyle='dashed')
 ax.plot(annual_rain_data.index, annual_rain_data["MA_20_Dub"], label='20yr MA Dub', linestyle='dashed')
 ax.set_xlabel('Year')
 ax.set_ylabel('Annual Rainfall (mm)')
-ax.set_title('Annual Rainfall, 1942 to 2020: Valencia and Dublin stations')
+ax.set_title('Annual Rainfall, 1942 to 2020: Valentia and Dublin stations')
 plt.legend()
 plt.show()
 
-#compare heavy rain days to rain days
-fig, ax = plt.subplots()
-ax.plot(annual_rain_data.index, annual_rain_data["heavyraindays_dub"], label='Heavy raindays Dub')
-ax.plot(annual_rain_data.index, annual_rain_data["heavyraindays_val"], label='Heavy raindays Val')
-ax.plot(annual_rain_data.index, annual_rain_data["raindays_dub"], label='Total raindays Dub')
-ax.plot(annual_rain_data.index, annual_rain_data["raindays_val"], label='Total raindays Val')
-ax.set_xlabel('Year')
-ax.set_ylabel('Raindays')
-ax.set_title('Rainfall Days and Heavy Rainfall Days, 1942 - 2020')
-plt.legend()
+#show heavy rain days to rain days Valentia
+fig, ax = plt.subplots(2, 1, sharex=True)
+fig.suptitle("Rainfall Days and Heavy Rainfall Days, 1942 - 2020, Valentia Station")
+ax[0].plot(annual_rain_data.index, annual_rain_data["heavyraindays_val"], marker="s", color="r")
+ax[0].set_ylabel("Heavy Rain Days\nDays Rain >=10mm")
+ax[1].plot(annual_rain_data.index, annual_rain_data["raindays_val"], marker="s", color="b")
+ax[1].set_ylabel("Rain Days\nDays Rain >=0.2mm")
 plt.show()
 
-#group data by season
-rain_data["month"] = rain_data.index.month
-print(rain_data.head())
+#show heavy rain days to rain days Dublin
+fig, ax = plt.subplots(2, 1, sharex=True)
+fig.suptitle("Rainfall Days and Heavy Rainfall Days, 1942 - 2020, Dublin Station")
+ax[0].plot(annual_rain_data.index, annual_rain_data["heavyraindays_dub"], marker="o", color="g")
+ax[0].set_ylabel("Heavy Rain Days\nDays Rain >=10mm")
+ax[1].plot(annual_rain_data.index, annual_rain_data["raindays_dub"], marker="o", color="m")
+ax[1].set_ylabel("Rain Days\nDays Rain >=0.2mm")
+plt.show()
+
+#Create season column
+SeasonDict = {12: "Winter", 1: "Winter", 2: "Winter", 3: "Spring", 4: "Spring", 5: "Spring", 6: "Summer", 7: "Summer", \
+              8: "Summer", 9: "Autumn", 10: "Autumn", 11: "Autumn"}
+rain_data["Season"] = rain_data["month"].map(SeasonDict)
+print(rain_data)
+
+#group data by season and create plots for each season
+
 
 
